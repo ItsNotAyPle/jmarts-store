77043fd7eb6de807528b7e33b044eaf453ac8ece
ayple <aypledev@gmail.com>
2023-03-30 03:04:57 +0100

    made a decent product page design

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -9,18 +9,25 @@ const ProductPage = ({product}:InferGetServerSidePropsType<typeof getServerSideP
     return (
         <Wrapper>
             <Navbar />
-            <div className="w-full h-full pt-24">
-                <ul className="w-full bg-black">
-                    <tr>
-                        <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
-                        <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
-                        <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
-                        <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
-                    </tr>
-                    <tr>
-
-                    </tr>
-                </ul>
+            <div className="w-full h-full flex flex-row pt-24 justify-center">
+                <div className="grid grid-cols-2 gap-4 w-fit px-24 ">
+                    <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
+                    <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
+                    <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />
+                    <img className="m-2 border-4 border-gray-600" src={`/images/${product.image_name_1}`} />    
+                </div>
+                <div className="w-1/2 mx-28 text-center">
+                    <h1 className="text-2xl font-bold">{product.title}</h1>
+                    <p className="mt-10 py-4  border-black">bdxbdsfhgdsh{product.description}</p>
+                    <form className="" action="/">
+                        <input type="hidden" name="id" value={product.product_id} />
+                        <select className="mt-10" name="sizes" id="sizes">
+                            <option value="small">small</option>
+                            <option value="medium">medium</option>
+                            <option value="large">large</option>
+                        </select>
+                    </form>
+                </div>
             </div>
         </Wrapper>
     );
